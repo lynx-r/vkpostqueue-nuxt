4be179480c0c9d99e7c86c429deb419ccd086b9f
lynx-r <lynx.p9@gmail.com>
2021-03-16 20:57:41 +0300

    fix

--- server-middleware/putPostToS3.ts
+++ server-middleware/putPostToS3.ts
@@ -13,7 +13,7 @@ const parseBody = <T>(req: IncomingMessage): Promise<T> => new Promise<string>(
   (resolve) => {
     const requestBody: Uint8Array[] = []
     req.on('data', chunk => requestBody.push(chunk))
-    req.on('end', () => resolve(Buffer.concat(requestBody).toString('utf8')))
+    req.on('end', () => resolve(Buffer.concat(requestBody).toString()))
   })
   .then(body => JSON.parse(body))
 
