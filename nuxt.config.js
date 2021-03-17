0b43c541f7ea2ff93e8ce5d4f214c6c220306628
lynx-r <lynx.p9@gmail.com>
2021-03-17 09:25:11 +0300

    env

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
 
   http: {
     // HTTP options here
-    baseUrl: process.env.VERCEL_URL || process.env.BASE_URL || 'http://localhost:3000'
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
   },
 
   publicRuntimeConfig: {
