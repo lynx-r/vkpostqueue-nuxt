0a8c297e05d5edde4e2cf6ffc818566c9de1dc47
lynx-r <lynx.p9@gmail.com>
2021-03-17 13:27:30 +0300

    err

--- server-middleware/putPostToS3_vercel.js
+++ server-middleware/putPostToS3_vercel.js
@@ -1,6 +1,6 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-const S3_BUCKET = process.env.S3_BUCKET
+const S3_BUCKET = process.env.S3_BUCKET;
 
 const s3 = new S3Client({
   credentials: {
@@ -8,14 +8,18 @@ const s3 = new S3Client({
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
   },
   region: process.env.S3_REGION
-})
+});
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
-    const putParams = {...req.body, Bucket: S3_BUCKET}
-    const putRes = await s3.send(new PutObjectCommand(putParams))
-    res.json(putRes)
-  } else {
-    res.status(405)
+  try {
+    if (req.method === 'POST') {
+      const putParams = {...req.body, Bucket: S3_BUCKET};
+      const putRes = await s3.send(new PutObjectCommand(putParams));
+      res.json(putRes);
+    } else {
+      res.status(405);
+    }
+  } catch (e) {
+    res.end(JSON.stringify(e));
   }
 }
