20ecc9fe7ac2d819e0128adf02bbf18ed1d55773
lynx-r <lynx.p9@gmail.com>
2021-03-17 13:38:49 +0300

    debug

--- server-middleware/putPostToS3_vercel.js
+++ server-middleware/putPostToS3_vercel.js
@@ -13,13 +13,16 @@ const s3 = new S3Client({
 export default async (req, res) => {
   try {
     if (req.method === 'POST') {
+      console.log(1);
+      console.log(-1, Object.getOwnPropertyNames(req));
       const putParams = {...req.body, Bucket: S3_BUCKET};
+      console.log(2, putParams);
       const putRes = await s3.send(new PutObjectCommand(putParams));
       res.json(putRes);
     } else {
       res.status(405);
     }
   } catch (e) {
-    res.end(JSON.stringify(e));
+    res.end(e.message);
   }
 }
