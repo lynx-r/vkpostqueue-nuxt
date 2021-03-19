8919fd58f84e28b5ca2638d58bbd7fad8308d7c0
lynx-r <lynx.p9@gmail.com>
2021-03-19 14:07:28 +0300

    vk implicit flow auth 🛑

--- pages/Home.vue
+++ pages/Home.vue
@@ -12,10 +12,10 @@ export default defineNuxtConfig({
   name: 'Home',
 
   async asyncData({$cookies}) {
-    const accessToken = '262dd552407b565f6c9afc3bc2d5602f7815f5528dee70c2ae0fc176d6ee36ec20dfbb6f796b7675f4792' //$cookies.get('accessToken');
+    const accessToken = '262dd552407b565f6c9afc3bc2d5602f7815f5528dee70c2ae0fc176d6ee36ec20dfbb6f796b7675f4792'; //$cookies.get('accessToken');
     console.log(accessToken);
     const vk = new VKAPI({accessToken: accessToken});
-    const res = await vk.wall.post({accessToken, ownerId: '-203255283', message: 'msg', fromGroup: 1})
+    const res = await vk.wall.post({accessToken, ownerId: '-203255283', message: 'msg', fromGroup: 1});
     console.log(res);
   },
   mounted() {
