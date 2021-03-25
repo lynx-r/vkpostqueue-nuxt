9ac0df303cb7357f37e95199053c970661375005
lynx-r <lynx.p9@gmail.com>
2021-03-25 21:25:17 +0300

    clean

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,8 @@
-import { defineNuxtMiddleware } from '@nuxtjs/composition-api';
-
-const auth = async ({redirect, $http, store, req, storage, $storage}) => {
+export default async ({redirect, $http, $storage}) => {
   const userId = $storage.getUniversal('userId')
-  console.log(userId);
-  const res = await $http.post('/api/isAuthenticated', {userId});
-  const auth = await res.json();
+  const res = await $http.post('/api/isAuthenticated', {userId})
+  const auth = await res.json()
   if (!auth?.isAuthenticated) {
-    return redirect({name: 'auth'});
+    return redirect({name: 'auth'})
   }
-};
-
-export default defineNuxtMiddleware(auth);
+}
