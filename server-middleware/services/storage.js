049a2e04e1f497782e06ecc47985b215c89d1c28
lynx-r <lynx.p9@gmail.com>
2021-04-13 13:58:13 +0300

    fix query

--- server-middleware/services/storage.js
+++ server-middleware/services/storage.js
@@ -4,33 +4,57 @@ import { FAUNADB_SECRET } from './constants'
 const client = new faunadb.Client({ secret: FAUNADB_SECRET })
 
 export const saveSubscription = (userId, subscription) => {
-  const ref = client
+  const existedSubscriptions = client
     .query(
-      q.Select('data',
-        q.Map(
-          q.Paginate(q.Documents(q.Collection('PushSubscribers'))),
-          q.Lambda(['ref'],
-            q.Select(['ref'], q.Get(q.Var('ref')))
-          )
+      q.Map(
+        q.Filter(
+          q.Select('data',
+            q.Map(
+              q.Paginate(q.Documents(q.Collection('PushSubscribers'))),
+              q.Lambda('ref',
+                q.Get(q.Var('ref'))
+              )
+            )
+          ),
+          q.Lambda('ref', q.Equals(userId, q.Select(['data', 'userId'], q.Var('ref'))))
+        ),
+        q.Lambda('ref', q.Select('ref', q.Var('ref')))
+      )
+    )
+    .catch(err => console.error('Error: %s', err))
+
+  if (!existedSubscriptions.length) {
+    const ref = existedSubscriptions[0]
+    client
+      .query(
+        q.Replace(
+          ref,
+          {
+            data: {
+              userId,
+              subscription
+            }
+          }
         )
       )
-    )
-    .catch(err => console.error('Error: %s', err))
-  console.log(ref)
-  client
-    .query(
-      q.Replace(
-        ref,
-        {
-          data: {
-            userId,
-            subscription
+      .then(ret => console.log(ret))
+      .catch(err => console.error('Error: %s', err))
+  } else {
+    client
+      .query(
+        q.Create(
+          q.Collection('PushSubscribers'),
+          {
+            data: {
+              userId,
+              subscription
+            }
           }
-        }
+        )
       )
-    )
-    .then(ret => console.log(ret))
-    .catch(err => console.error('Error: %s', err))
+      .then(ret => console.log(ret))
+      .catch(err => console.error('Error: %s', err))
+  }
 }
 
 export const getSubscriptions = async () => {
